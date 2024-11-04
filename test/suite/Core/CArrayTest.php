f6465b9b17c51ea72f8b671b91cd8473d6dd0684
eylemugurel <eylemugurel@hotmail.com>
2024-11-04 08:43:51 +0100

    CArray: Add InsertBefore, InsertAfter tests

--- test/suite/Core/CArrayTest.php
+++ test/suite/Core/CArrayTest.php
@@ -149,6 +149,180 @@ class CArrayTest extends TestCase
 
     #endregion PopFront
 
+    #region InsertBefore -------------------------------------------------------
+
+    function testInsertBeforeInMiddle()
+    {
+        $carr = new CArray([100, 102, 103]);
+        $carr->InsertBefore(1, 101);
+        $this->assertSame([100, 101, 102, 103],
+            AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    function testInsertBeforeAtBeginning()
+    {
+        $carr = new CArray([101, 102, 103]);
+        $carr->InsertBefore(0, 100);
+        $this->assertSame([100, 101, 102, 103],
+            AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    function testInsertBeforeAtEnd()
+    {
+        $carr = new CArray([100, 101, 102]);
+        $carr->InsertBefore(3, 103);
+        $this->assertSame([100, 101, 102, 103],
+            AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    function testInsertBeforeChaining()
+    {
+        $carr = new CArray([100, 103]);
+        $carr->InsertBefore(1, 101)->InsertBefore(2, 102);
+        $this->assertSame([100, 101, 102, 103],
+            AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    function testInsertBeforeWithNegativeOffset()
+    {
+        $carr = new CArray([100, 101, 102]);
+        $this->expectException(\OutOfRangeException::class);
+        $carr->InsertBefore(-1, 99);
+    }
+
+    function testInsertBeforeWithOffsetExceedingSize()
+    {
+        $carr = new CArray([100, 101, 102]);
+        $this->expectException(\OutOfRangeException::class);
+        $carr->InsertBefore(4, 103);
+    }
+
+    function testInsertBeforeReindexKeys()
+    {
+        $carr = new CArray([10 => 100, 11 => 101, 13 => 103]);
+        $carr->InsertBefore(2, 102);
+        $this->assertSame([0 => 100, 1 => 101, 2 => 102, 3 => 103],
+            AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    function testInsertBeforeWithAssociativeArray()
+    {
+        $carr = new CArray(['ten' => 10, 'eleven' => 11, 'thirteen' => 13]);
+        $carr->InsertBefore(2, 12);
+        $this->assertSame([
+            'ten' => 10,
+            'eleven' => 11,
+            0 => 12,
+            'thirteen' => 13
+        ], AccessHelper::GetNonPublicProperty($carr, 'value'));
+        $carr->InsertBefore(1, 10.5);
+        $this->assertSame([
+            'ten' => 10,
+            0 => 10.5,
+            'eleven' => 11,
+            1 => 12,
+            'thirteen' => 13
+        ], AccessHelper::GetNonPublicProperty($carr, 'value'));
+        $carr->InsertBefore(4, 12.5);
+        $this->assertSame([
+            'ten' => 10,
+            0 => 10.5,
+            'eleven' => 11,
+            1 => 12,
+            2 => 12.5,
+            'thirteen' => 13
+        ], AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    #endregion InsertBefore
+
+    #region InsertAfter --------------------------------------------------------
+
+    function testInsertAfterInMiddle()
+    {
+        $carr = new CArray([100, 101, 103]);
+        $carr->InsertAfter(1, 102);
+        $this->assertSame([100, 101, 102, 103],
+            AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    function testInsertAfterAtBeginning()
+    {
+        $carr = new CArray([101, 102, 103]);
+        $carr->InsertAfter(0, 100);
+        $this->assertSame([101, 100, 102, 103],
+            AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    function testInsertAfterAtEnd()
+    {
+        $carr = new CArray([100, 101, 102]);
+        $carr->InsertAfter(2, 103);
+        $this->assertSame([100, 101, 102, 103],
+            AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    function testInsertAfterChaining()
+    {
+        $carr = new CArray([100, 101]);
+        $carr->InsertAfter(0, 100.5)->InsertAfter(2, 101.5);
+        $this->assertSame([100, 100.5, 101, 101.5],
+            AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    function testInsertAfterWithNegativeOffset()
+    {
+        $carr = new CArray([100, 101, 102]);
+        $this->expectException(\OutOfRangeException::class);
+        $carr->InsertAfter(-1, 99);
+    }
+
+    function testInsertAfterWithOffsetExceedingSize()
+    {
+        $carr = new CArray([100, 101, 102]);
+        $this->expectException(\OutOfRangeException::class);
+        $carr->InsertAfter(3, 103);
+    }
+
+    function testInsertAfterReindexKeys()
+    {
+        $carr = new CArray([10 => 100, 11 => 101, 13 => 103]);
+        $carr->InsertAfter(1, 102);
+        $this->assertSame([0 => 100, 1 => 101, 2 => 102, 3 => 103],
+            AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    function testInsertAfterWithAssociativeArray()
+    {
+        $carr = new CArray(['ten' => 10, 'eleven' => 11, 'thirteen' => 13]);
+        $carr->InsertAfter(1, 12);
+        $this->assertSame([
+            'ten' => 10,
+            'eleven' => 11,
+            0 => 12,
+            'thirteen' => 13
+        ], AccessHelper::GetNonPublicProperty($carr, 'value'));
+        $carr->InsertAfter(0, 10.5);
+        $this->assertSame([
+            'ten' => 10,
+            0 => 10.5,
+            'eleven' => 11,
+            1 => 12,
+            'thirteen' => 13
+        ], AccessHelper::GetNonPublicProperty($carr, 'value'));
+        $carr->InsertAfter(3, 12.5);
+        $this->assertSame([
+            'ten' => 10,
+            0 => 10.5,
+            'eleven' => 11,
+            1 => 12,
+            2 => 12.5,
+            'thirteen' => 13
+        ], AccessHelper::GetNonPublicProperty($carr, 'value'));
+    }
+
+    #endregion InsertAfter
+
     #region Data Providers -----------------------------------------------------
 
     static function containsKeyDataProvider()
