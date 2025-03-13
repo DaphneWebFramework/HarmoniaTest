8f9b8106963bd455ba1b1e246475447cc4c7b659
eylemugurel <eylemugurel@hotmail.com>
2025-03-13 18:29:42 +0100

    Add Requirement tests (Validation)

---
+++ test/backend/suite/Validation/Requirements/FieldRequirementConstraintsTest.php
@@ -0,0 +1,111 @@
+<?php declare(strict_types=1);
+use \PHPUnit\Framework\TestCase;
+use \PHPUnit\Framework\Attributes\CoversClass;
+
+use \Harmonia\Validation\Requirements\FieldRequirementConstraints;
+
+use \Harmonia\Config;
+use \Harmonia\Validation\MetaRules\IMetaRule;
+
+#[CoversClass(FieldRequirementConstraints::class)]
+class FieldRequirementConstraintsTest extends TestCase
+{
+    private ?Config $originalConfig = null;
+
+    protected function setUp(): void
+    {
+        $this->originalConfig = Config::ReplaceInstance($this->config());
+    }
+
+    protected function tearDown(): void
+    {
+        Config::ReplaceInstance($this->originalConfig);
+    }
+
+    private function config()
+    {
+        $mock = $this->createMock(Config::class);
+        $mock->method('Option')->with('Language')->willReturn('en');
+        return $mock;
+    }
+
+    private function metaRule(string $name, mixed $param = null): IMetaRule
+    {
+        $mock = $this->createMock(IMetaRule::class);
+        $mock->method('GetName')->willReturn($name);
+        $mock->method('GetParam')->willReturn($param);
+        return $mock;
+    }
+
+    #region FromMetaRules ------------------------------------------------------
+
+    function testFromMetaRulesExtractsRequiredRule()
+    {
+        $sut = FieldRequirementConstraints::FromMetaRules([
+            $this->metaRule('required')
+        ]);
+
+        $this->assertTrue($sut->IsRequired());
+        $this->assertFalse($sut->HasRequiredWithoutFields());
+        $this->assertEmpty($sut->RequiredWithoutFields());
+    }
+
+    function testFromMetaRulesExtractsRequiredWithoutRule()
+    {
+        $sut = FieldRequirementConstraints::FromMetaRules([
+            $this->metaRule('requiredwithout', 'email')
+        ]);
+
+        $this->assertFalse($sut->IsRequired());
+        $this->assertTrue($sut->HasRequiredWithoutFields());
+        $this->assertSame(['email'], $sut->RequiredWithoutFields());
+    }
+
+    function testFromMetaRulesExtractsBothRequiredAndRequiredWithoutRules()
+    {
+        $sut = FieldRequirementConstraints::FromMetaRules([
+            $this->metaRule('required'),
+            $this->metaRule('requiredwithout', 'email')
+        ]);
+
+        $this->assertTrue($sut->IsRequired());
+        $this->assertTrue($sut->HasRequiredWithoutFields());
+        $this->assertSame(['email'], $sut->RequiredWithoutFields());
+    }
+
+    function testFromMetaRulesThrowsIfRequiredWithoutRuleHasNoFieldName()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            "Rule 'requiredWithout' must be used with a field name.");
+
+        FieldRequirementConstraints::FromMetaRules([
+            $this->metaRule('requiredwithout')
+        ]);
+    }
+
+    #endregion FromMetaRules
+
+    #region FormatRequiredWithoutList ------------------------------------------
+
+    function testFormatRequiredWithoutListWithSingleField()
+    {
+        $sut = FieldRequirementConstraints::FromMetaRules([
+            $this->metaRule('requiredwithout', 'email')
+        ]);
+
+        $this->assertSame("'email'", $sut->FormatRequiredWithoutList());
+    }
+
+    function testFormatRequiredWithoutListWithMultipleFields()
+    {
+        $sut = FieldRequirementConstraints::FromMetaRules([
+            $this->metaRule('requiredwithout', 'email'),
+            $this->metaRule('requiredwithout', 'phone')
+        ]);
+
+        $this->assertSame("one of 'email', 'phone'", $sut->FormatRequiredWithoutList());
+    }
+
+    #endregion FormatRequiredWithoutList
+}
---
+++ test/backend/suite/Validation/Requirements/RequirementEngineTest.php
@@ -0,0 +1,233 @@
+<?php declare(strict_types=1);
+use \PHPUnit\Framework\TestCase;
+use \PHPUnit\Framework\Attributes\CoversClass;
+use \PHPUnit\Framework\Attributes\DataProvider;
+
+use \Harmonia\Validation\Requirements\RequirementEngine;
+
+use \Harmonia\Config;
+use \Harmonia\Validation\DataAccessor;
+use \Harmonia\Validation\MetaRules\IMetaRule;
+
+#[CoversClass(RequirementEngine::class)]
+class RequirementEngineTest extends TestCase
+{
+    private ?Config $originalConfig = null;
+
+    protected function setUp(): void
+    {
+        $this->originalConfig = Config::ReplaceInstance($this->config());
+    }
+
+    protected function tearDown(): void
+    {
+        Config::ReplaceInstance($this->originalConfig);
+    }
+
+    private function config()
+    {
+        $mock = $this->createMock(Config::class);
+        $mock->method('Option')->with('Language')->willReturn('en');
+        return $mock;
+    }
+
+    private function metaRule(string $name, mixed $param = null): IMetaRule
+    {
+        $mock = $this->createMock(IMetaRule::class);
+        $mock->method('GetName')->willReturn($name);
+        $mock->method('GetParam')->willReturn($param);
+        return $mock;
+    }
+
+    private function metaRules(array $rules): array
+    {
+        return \array_map(
+            fn($rule) => $this->metaRule(...$rule),
+            $rules
+        );
+    }
+
+    private function dataAccessor(array $fields = []): DataAccessor
+    {
+        $mock = $this->createMock(DataAccessor::class);
+        $mock->method('HasField')->willReturnCallback(
+            fn($field) => \array_key_exists($field, $fields));
+        return $mock;
+    }
+
+    #region Validate -----------------------------------------------------------
+
+    #[DataProvider('validateDataProvider')]
+    function testValidate(
+        ?string $exceptionMessage,
+        string $field,
+        array $rules,
+        array $data
+    ) {
+        // Exceptions can be thrown both from the constructor and the Validate.
+        if ($exceptionMessage !== null) {
+            // Can be either an InvalidArgumentException or a RuntimeException.
+            $this->expectException(\Exception::class);
+            $this->expectExceptionMessage($exceptionMessage);
+        }
+
+        $sut = new RequirementEngine(
+            $field,
+            $this->metaRules($rules),
+            $this->dataAccessor($data)
+        );
+        $sut->Validate();
+
+        if ($exceptionMessage === null) {
+            $this->expectNotToPerformAssertions();
+        }
+    }
+
+    #endregion Validate
+
+    #region ShouldSkipFurtherValidation ----------------------------------------
+
+    #[DataProvider('shouldSkipFurtherValidationDataProvider')]
+    function testShouldSkipFurtherValidation(
+        bool $expected,
+        string $field,
+        array $rules,
+        array $data
+    ) {
+        $sut = new RequirementEngine(
+            $field,
+            $this->metaRules($rules),
+            $this->dataAccessor($data)
+        );
+
+        $this->assertSame($expected, $sut->ShouldSkipFurtherValidation());
+    }
+
+    #endregion ShouldSkipFurtherValidation
+
+    #region FilterOutRequirementRules ------------------------------------------
+
+    function testFilterOutRequirementRules()
+    {
+        $metaRules = [
+            $this->metaRule('required'),
+            $this->metaRule('requiredwithout', 'someField'),
+            $this->metaRule('string')
+        ];
+        $sut = new RequirementEngine(
+            'myField',
+            $metaRules,
+            $this->dataAccessor()
+        );
+
+        $filteredRules = $sut->FilterOutRequirementRules($metaRules);
+
+        $this->assertCount(1, $filteredRules);
+        $this->assertSame(
+            'string',
+            $filteredRules[\array_key_first($filteredRules)]->GetName()
+        );
+    }
+
+    #endregion FilterOutRequirementRules
+
+    #region Data Providers -----------------------------------------------------
+
+    static function validateDataProvider()
+    {
+        return [
+            'Passes when there are no validation rules' => [
+                null,            // exception message
+                'myField',       // field
+                [],              // rules
+                ['myField' => 1] // data
+            ],
+            'Passes when required field is present' => [
+                null,
+                'myField',
+                [['required']],
+                ['myField' => 1]
+            ],
+            'Passes when requiredWithout field is present' => [
+                null,
+                'myField',
+                [['requiredwithout', 'otherField']],
+                ['otherField' => 1]
+            ],
+            'Passes when at least one requiredWithout field is present' => [
+                null,
+                'myField',
+                [['requiredwithout', 'otherField1'], ['requiredwithout', 'otherField2']],
+                ['otherField2' => 1]
+            ],
+            'Fails when field and its requiredWithout field both exist' => [
+                "Only one of fields 'myField' or 'otherField' can be present.",
+                'myField',
+                [['requiredwithout', 'otherField']],
+                ['myField' => 1, 'otherField' => 1]
+            ],
+            'Fails when required and requiredWithout rules exist, and field is present' => [
+                "Only one of fields 'myField' or 'otherField' can be present.",
+                'myField',
+                [['required'], ['requiredwithout', 'otherField']],
+                ['myField' => 1, 'otherField' => 1]
+            ],
+            'Fails when required field is missing despite a requiredWithout field' => [
+                "Required field 'myField' is missing.",
+                'myField',
+                [['required'], ['requiredwithout', 'otherField']],
+                ['otherField' => 1]
+            ],
+            'Fails when required field is absent' => [
+                "Required field 'myField' is missing.",
+                'myField',
+                [['required']],
+                []
+            ],
+            'Fails when requiredWithout field is absent' => [
+                "Either field 'myField' or 'otherField' must be present.",
+                'myField',
+                [['requiredwithout', 'otherField']],
+                []
+            ],
+            'Fails when all requiredWithout fields are missing' => [
+                "Either field 'myField' or one of 'otherField1', 'otherField2' must be present.",
+                'myField',
+                [['requiredwithout', 'otherField1'], ['requiredwithout', 'otherField2']],
+                []
+            ],
+            'Fails when requiredWithout rule has an empty field name' => [
+                "Rule 'requiredWithout' must be used with a field name.",
+                'myField',
+                [['requiredwithout', null]],
+                []
+            ],
+            'Fails when requiredWithout rule refers to the same field' => [
+                "Rule 'requiredWithout' must not reference the field itself.",
+                'myField',
+                [['requiredwithout', 'myField']],
+                []
+            ],
+        ];
+    }
+
+    static function shouldSkipFurtherValidationDataProvider()
+    {
+        return [
+            'Does not skip when field is present' => [
+                false, 'myField', [['required']], ['myField' => 1]
+            ],
+            'Skips when field is missing and requiredWithout field exists' => [
+                true, 'myField', [['requiredwithout', 'otherField']], ['otherField' => 1]
+            ],
+            'Skips when field is missing and is not required' => [
+                true, 'myField', [], []
+            ],
+            'Does not skip when field is missing but required' => [
+                false, 'myField', [['required']], []
+            ]
+        ];
+    }
+
+    #endregion Data Providers
+}
