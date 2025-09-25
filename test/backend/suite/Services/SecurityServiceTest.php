1fec2b71ba42f10307d33196ab336001fd760ae4
eylemugurel <eylemugurel@hotmail.com>
2025-09-25 13:04:34 +0200

    SecurityService: Switch to HMAC for CSRF token generation/verification

--- test/backend/suite/Services/SecurityServiceTest.php
+++ test/backend/suite/Services/SecurityServiceTest.php
@@ -1,18 +1,32 @@
 <?php declare(strict_types=1);
 use \PHPUnit\Framework\TestCase;
 use \PHPUnit\Framework\Attributes\CoversClass;
-use \PHPUnit\Framework\Attributes\DataProvider;
+use \PHPUnit\Framework\Attributes\DataProviderExternal;
 
 use \Harmonia\Services\SecurityService;
 
-use \Harmonia\Services\Security\CsrfToken;
+use \Harmonia\Config;
 use \TestToolkit\AccessHelper;
+use \TestToolkit\DataHelper;
 
 #[CoversClass(SecurityService::class)]
 class SecurityServiceTest extends TestCase
 {
-    private const DEFAULT_TOKEN_PATTERN = '/^[a-f0-9]{64}$/';
-    private const CSRF_TOKEN_COOKIE_VALUE_PATTERN = '/^[a-f0-9]{120}$/';
+    private const DEFAULT_TOKEN_PATTERN = '/^[A-Fa-f0-9]{64}$/';
+    private const CSRF_COOKIE_VALUE_PATTERN = '/^[A-Fa-f0-9]{64}$/';
+
+    private ?Config $originalConfig = null;
+
+    protected function setUp(): void
+    {
+        $this->originalConfig =
+            Config::ReplaceInstance($this->createMock(Config::class));
+    }
+
+    protected function tearDown(): void
+    {
+        Config::ReplaceInstance($this->originalConfig);
+    }
 
     private function systemUnderTest(string ...$mockedMethods): SecurityService
     {
@@ -24,19 +38,12 @@ class SecurityServiceTest extends TestCase
 
     #region HashPassword -------------------------------------------------------
 
-    function testHashPasswordWithEmptyPassword()
-    {
-        $sut = $this->systemUnderTest();
-        $password = '';
-        $hash = $sut->HashPassword($password);
-        $this->assertTrue(\password_verify($password, $hash));
-    }
-
-    function testHashPasswordWithNonEmptyPassword()
+    function testHashPassword()
     {
         $sut = $this->systemUnderTest();
         $password = 'pass1234';
         $hash = $sut->HashPassword($password);
+
         $this->assertTrue(\password_verify($password, $hash));
     }
 
@@ -44,28 +51,15 @@ class SecurityServiceTest extends TestCase
 
     #region VerifyPassword -----------------------------------------------------
 
-    function testVerifyPasswordWithInvalidPassword()
+    function testVerifyPassword()
     {
         $sut = $this->systemUnderTest();
         $password = 'pass1234';
         $hash = \password_hash($password, \PASSWORD_DEFAULT);
+
+        $this->assertTrue($sut->VerifyPassword($password, $hash));
         $this->assertFalse($sut->VerifyPassword('invalid', $hash));
-    }
-
-    function testVerifyPasswordWithEmptyPassword()
-    {
-        $sut = $this->systemUnderTest();
-        $password = '';
-        $hash = \password_hash($password, \PASSWORD_DEFAULT);
-        $this->assertTrue($sut->VerifyPassword($password, $hash));
-    }
-
-    function testVerifyPasswordWithNonEmptyPassword()
-    {
-        $sut = $this->systemUnderTest();
-        $password = 'pass1234';
-        $hash = \password_hash($password, \PASSWORD_DEFAULT);
-        $this->assertTrue($sut->VerifyPassword($password, $hash));
+        $this->assertFalse($sut->VerifyPassword($password, 'invalid'));
     }
 
     #endregion VerifyPassword
@@ -83,7 +77,7 @@ class SecurityServiceTest extends TestCase
     {
         $sut = $this->systemUnderTest();
         $token = $sut->GenerateToken(8);
-        $this->assertMatchesRegularExpression('/^[a-f0-9]{16}$/', $token);
+        $this->assertMatchesRegularExpression('/^[A-Fa-f0-9]{16}$/', $token);
     }
 
     #endregion GenerateToken
@@ -101,126 +95,104 @@ class SecurityServiceTest extends TestCase
     {
         $sut = $this->systemUnderTest();
         $tokenPattern = $sut->TokenPattern(8);
-        $this->assertSame('/^[a-f0-9]{16}$/', $tokenPattern);
+        $this->assertSame('/^[A-Fa-f0-9]{16}$/', $tokenPattern);
     }
 
     #endregion TokenPattern
 
-    #region GenerateCsrfToken --------------------------------------------------
+    #region GenerateCsrfPair ---------------------------------------------------
 
-    function testGenerateCsrfToken()
+    function testGenerateCsrfPair()
     {
-        $sut = $this->systemUnderTest();
-        $csrfToken = $sut->GenerateCsrfToken();
+        $sut = $this->systemUnderTest('csrfSecret');
+
+        $sut->expects($this->once())
+            ->method('csrfSecret')
+            ->willReturn('12345678901234567890123456789012');
+
+        [$token, $cookieValue] = $sut->GenerateCsrfPair();
+
         $this->assertMatchesRegularExpression(
             self::DEFAULT_TOKEN_PATTERN,
-            $csrfToken->Token()
+            $token
         );
         $this->assertMatchesRegularExpression(
-            self::CSRF_TOKEN_COOKIE_VALUE_PATTERN,
-            $csrfToken->CookieValue()
+            self::CSRF_COOKIE_VALUE_PATTERN,
+            $cookieValue
         );
-        $deobfuscatedCookieValue = AccessHelper::CallMethod(
-            $sut,
-            'deobfuscate',
-            [$csrfToken->CookieValue()]
+    }
+
+    #endregion GenerateCsrfPair
+
+    #region VerifyCsrfPair -----------------------------------------------------
+
+    function testVerifyCsrfPair()
+    {
+        $sut = $this->systemUnderTest('csrfSecret');
+
+        $sut->expects($this->any())
+            ->method('csrfSecret')
+            ->willReturn('12345678901234567890123456789012');
+
+        [$token, $cookieValue] = $sut->GenerateCsrfPair();
+
+        $this->assertTrue($sut->VerifyCsrfPair($token, $cookieValue));
+        $this->assertFalse($sut->VerifyCsrfPair('invalid', $cookieValue));
+        $this->assertFalse($sut->VerifyCsrfPair($token, 'invalid'));
+    }
+
+    #endregion VerifyCsrfPair
+
+    #region csrfSecret ---------------------------------------------------------
+
+    #[DataProviderExternal(DataHelper::class, 'NonStringProvider')]
+    function testCsrfSecretWhenValueIsNotString($value)
+    {
+        $sut = $this->systemUnderTest();
+        $config = Config::Instance();
+
+        $config->expects($this->once())
+            ->method('Option')
+            ->with('CsrfSecret')
+            ->willReturn($value);
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage(
+            'CSRF secret must be a string of at least 32 characters.');
+        AccessHelper::CallMethod($sut, 'csrfSecret');
+    }
+
+    function testCsrfSecretWhenValueIsTooShort()
+    {
+        $sut = $this->systemUnderTest();
+        $config = Config::Instance();
+
+        $config->expects($this->once())
+            ->method('Option')
+            ->with('CsrfSecret')
+            ->willReturn('1234567890'); // less than 32 characters
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage(
+            'CSRF secret must be a string of at least 32 characters.');
+        AccessHelper::CallMethod($sut, 'csrfSecret');
+    }
+
+    function testCsrfSecretWhenValueIsValid()
+    {
+        $sut = $this->systemUnderTest();
+        $config = Config::Instance();
+
+        $config->expects($this->once())
+            ->method('Option')
+            ->with('CsrfSecret')
+            ->willReturn('12345678901234567890123456789012');
+
+        $this->assertSame(
+            '12345678901234567890123456789012',
+            AccessHelper::CallMethod($sut, 'csrfSecret')
         );
-        $this->assertTrue(\password_verify(
-            $csrfToken->Token(),
-            $deobfuscatedCookieValue
-        ));
     }
 
-    #endregion GenerateCsrfToken
-
-    #region VerifyCsrfToken ----------------------------------------------------
-
-    function testVerifyCsrfTokenWithEmptyTokenAndEmptyCookieValue()
-    {
-        $sut = $this->systemUnderTest();
-        $csrfToken = new CsrfToken('', '');
-        $this->assertFalse($sut->VerifyCsrfToken($csrfToken));
-    }
-
-    function testVerifyCsrfTokenWithInvalidTokenAndInvalidCookieValue()
-    {
-        $sut = $this->systemUnderTest();
-        $csrfToken = new CsrfToken('invalid', 'invalid');
-        $this->assertFalse($sut->VerifyCsrfToken($csrfToken));
-    }
-
-    function testVerifyCsrfTokenWithValidTokenAndEmptyCookieValue()
-    {
-        $sut = $this->systemUnderTest();
-        $csrfToken = new CsrfToken($sut->GenerateToken(), '');
-        $this->assertFalse($sut->VerifyCsrfToken($csrfToken));
-    }
-
-    function testVerifyCsrfTokenWithValidTokenAndInvalidCookieValue()
-    {
-        $sut = $this->systemUnderTest();
-        // Odd number of characters.
-        $csrfToken = new CsrfToken($sut->GenerateToken(), 'invalid');
-        $this->assertFalse($sut->VerifyCsrfToken($csrfToken));
-        // Even number of characters.
-        $csrfToken = new CsrfToken($sut->GenerateToken(), 'invalid0');
-        $this->assertFalse($sut->VerifyCsrfToken($csrfToken));
-    }
-
-    function testVerifyCsrfTokenWithValidTokenAndValidCookieValue()
-    {
-        $sut = $this->systemUnderTest();
-        $csrfToken = $sut->GenerateCsrfToken();
-        $this->assertTrue($sut->VerifyCsrfToken($csrfToken));
-    }
-
-    #endregion VerifyCsrfToken
-
-    #region obfuscate ----------------------------------------------------------
-
-    #[DataProvider('obfuscateDataProvider')]
-    public function testObfuscate($expected, $input)
-    {
-        $sut = $this->systemUnderTest();
-        $actual = AccessHelper::CallMethod($sut, 'obfuscate', [$input]);
-        $this->assertSame($expected, $actual);
-    }
-
-    #endregion obfuscate
-
-    #region deobfuscate --------------------------------------------------------
-
-    #[DataProvider('deobfuscateDataProvider')]
-    public function testDeobfuscate($expected, $input)
-    {
-        $sut = $this->systemUnderTest();
-        $actual = AccessHelper::CallMethod($sut, 'deobfuscate', [$input]);
-        $this->assertSame($expected, $actual);
-    }
-
-    #endregion deobfuscate
-
-    #region Data Providers -----------------------------------------------------
-
-    static function obfuscateDataProvider()
-    {
-        return [
-            'empty' => ['', ''],
-            'non-empty' => ['3433323173736170', 'pass1234'],
-        ];
-    }
-
-    static function deobfuscateDataProvider()
-    {
-        return [
-            'empty' => ['', ''],
-            'non-hex' => ['', 'hello'],
-            'non-even' => ['', '12345'],
-            'lowercase-hex' => ['hello', '6f6c6c6568'],
-            'uppercase-hex' => ['hello', '6F6C6C6568'],
-            'valid' => ['pass1234', '3433323173736170'],
-        ];
-    }
-
-    #endregion Data Providers
+    #endregion csrfSecret
 }
