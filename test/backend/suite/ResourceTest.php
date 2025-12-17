e6460ff40dacccd07909ef0c20f092c86815fc4c
eylemugurel <eylemugurel@hotmail.com>
2025-12-17 21:48:48 +0100

    Resource: Prohibit mutation of internal state through returned objects

--- test/backend/suite/ResourceTest.php
+++ test/backend/suite/ResourceTest.php
@@ -108,6 +108,20 @@ class ResourceTest extends TestCase
         $sut->AppPath();
     }
 
+    function testAppPathReturnsClone()
+    {
+        $sut = $this->systemUnderTest();
+
+        $sut->Initialize(__DIR__);
+        $path1 = $sut->AppPath();
+        $path1->AppendInPlace('/oops');
+        $path2 = $sut->AppPath();
+
+        $this->assertNotSame($path1, $path2);
+        $this->assertNotEquals($path1, $path2);
+        $this->assertEquals(__DIR__, $path2);
+    }
+
     #endregion AppPath
 
     #region AppRelativePath ----------------------------------------------------
@@ -223,6 +237,34 @@ class ResourceTest extends TestCase
         $this->assertEquals('Core', $sut->AppRelativePath());
     }
 
+    function testAppRelativePathReturnsClone()
+    {
+        $sut = $this->systemUnderTest('AppPath');
+        $server = Server::Instance();
+        $serverPath = new CPath(__DIR__);
+
+        $sut->expects($this->once())
+            ->method('AppPath')
+            ->willReturn($serverPath->Extend('app'));
+        $server->expects($this->once())
+            ->method('Path')
+            ->willReturn($serverPath);
+
+        $path1 = $sut->AppRelativePath();
+        $path1->ReplaceInPlace('app', 'oops1');
+        $path2 = $sut->AppRelativePath();
+        $path2->ReplaceInPlace('app', 'oops2');
+        $path3 = $sut->AppRelativePath();
+
+        $this->assertNotSame($path1, $path2);
+        $this->assertNotSame($path2, $path3);
+        $this->assertNotEquals($path1, $path2);
+        $this->assertNotEquals($path2, $path3);
+        $this->assertEquals('oops1', $path1);
+        $this->assertEquals('oops2', $path2);
+        $this->assertEquals('app', $path3);
+    }
+
     #endregion AppRelativePath
 
     #region AppUrl -------------------------------------------------------------
@@ -333,6 +375,33 @@ class ResourceTest extends TestCase
         $this->assertEquals('http://localhost/Core/', $sut->AppUrl());
     }
 
+    function testAppUrlReturnsClone()
+    {
+        $sut = $this->systemUnderTest('AppRelativePath');
+        $server = Server::Instance();
+
+        $server->expects($this->once())
+            ->method('Url')
+            ->willReturn(new CUrl('url/to/server'));
+        $sut->expects($this->once())
+            ->method('AppRelativePath')
+            ->willReturn(new CPath('app'));
+
+        $url1 = $sut->AppUrl();
+        $url1->ReplaceInPlace('app', 'oops1');
+        $url2 = $sut->AppUrl();
+        $url2->ReplaceInPlace('app', 'oops2');
+        $url3 = $sut->AppUrl();
+
+        $this->assertNotSame($url1, $url2);
+        $this->assertNotSame($url2, $url3);
+        $this->assertNotEquals($url1, $url3);
+        $this->assertNotEquals($url2, $url3);
+        $this->assertEquals('url/to/server/oops1/', $url1);
+        $this->assertEquals('url/to/server/oops2/', $url2);
+        $this->assertEquals('url/to/server/app/', $url3);
+    }
+
     #endregion AppUrl
 
     #region AppSubdirectoryPath ------------------------------------------------
@@ -357,6 +426,29 @@ class ResourceTest extends TestCase
         );
     }
 
+    function testAppSubdirectoryPathReturnsClone()
+    {
+        $sut = $this->systemUnderTest('AppPath');
+
+        $sut->expects($this->once())
+            ->method('AppPath')
+            ->willReturn(new CPath('path/to/app/'));
+
+        $path1 = $sut->AppSubdirectoryPath('subdir');
+        $path1->ReplaceInPlace('subdir', 'oops1');
+        $path2 = $sut->AppSubdirectoryPath('subdir');
+        $path2->ReplaceInPlace('subdir', 'oops2');
+        $path3 = $sut->AppSubdirectoryPath('subdir');
+
+        $this->assertNotSame($path1, $path2);
+        $this->assertNotSame($path2, $path3);
+        $this->assertNotEquals($path1, $path2);
+        $this->assertNotEquals($path2, $path3);
+        $this->assertEquals('path/to/app/oops1', $path1);
+        $this->assertEquals('path/to/app/oops2', $path2);
+        $this->assertEquals('path/to/app/subdir', $path3);
+    }
+
     #endregion AppSubdirectoryPath
 
     #region AppSubdirectoryUrl -------------------------------------------------
@@ -379,6 +471,29 @@ class ResourceTest extends TestCase
             $expected,
             AccessHelper::CallMethod($sut, 'AppSubdirectoryUrl', ['subdir'])
         );
+    }
+
+    function testAppSubdirectoryUrlReturnsClone()
+    {
+        $sut = $this->systemUnderTest('AppUrl');
+
+        $sut->expects($this->once())
+            ->method('AppUrl')
+            ->willReturn(new CUrl('url/to/app/'));
+
+        $url1 = $sut->AppSubdirectoryUrl('subdir');
+        $url1->ReplaceInPlace('subdir', 'oops1');
+        $url2 = $sut->AppSubdirectoryUrl('subdir');
+        $url2->ReplaceInPlace('subdir', 'oops2');
+        $url3 = $sut->AppSubdirectoryUrl('subdir');
+
+        $this->assertNotSame($url1, $url2);
+        $this->assertNotSame($url2, $url3);
+        $this->assertNotEquals($url1, $url2);
+        $this->assertNotEquals($url2, $url3);
+        $this->assertEquals('url/to/app/oops1', $url1);
+        $this->assertEquals('url/to/app/oops2', $url2);
+        $this->assertEquals('url/to/app/subdir', $url3);
     }
 
     #endregion AppSubdirectoryUrl
