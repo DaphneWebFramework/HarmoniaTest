a5850641db101cd46190826ec3638ae17e18d1dd
eylemugurel <eylemugurel@hotmail.com>
2025-12-24 10:42:57 +0100

    Config: Refactor Load() to accept string paths instead of CPath

--- test/backend/suite/ConfigTest.php
+++ test/backend/suite/ConfigTest.php
@@ -4,27 +4,24 @@ use \PHPUnit\Framework\Attributes\CoversClass;
 
 use \Harmonia\Config;
 
-use \Harmonia\Core\CPath;
 use \Harmonia\Core\CFileSystem;
-use \TestToolkit\AccessHelper;
+use \TestToolkit\AccessHelper as ah;
 
 #[CoversClass(Config::class)]
 class ConfigTest extends TestCase
 {
-    private ?Config $originalConfig = null;
-    private CPath $testFilePath;
+    private string $testFilePath;
 
     protected function setUp(): void
     {
-        $this->originalConfig = Config::ReplaceInstance(null);
-        $this->testFilePath = CPath::Join(__DIR__, 'config_test.inc.php');
+        $this->testFilePath = __DIR__ . '/config_test.inc.php';
     }
 
     protected function tearDown(): void
     {
-        Config::ReplaceInstance($this->originalConfig);
-        if ($this->testFilePath->Call('\is_file')) {
-            $this->assertTrue(CFileSystem::Instance()->DeleteFile($this->testFilePath));
+        if (\is_file($this->testFilePath)) {
+            $fileSystem = CFileSystem::Instance();
+            $this->assertTrue($fileSystem->DeleteFile($this->testFilePath));
         }
     }
 
@@ -38,19 +35,28 @@ class ConfigTest extends TestCase
      */
     private function createTestFile(string $fileContent): void
     {
-        $this->testFilePath->Call(
-            '\file_put_contents',
+        \file_put_contents(
+            $this->testFilePath,
             \preg_replace('/^[ ]{4}/m', '', $fileContent)
         );
+    }
+
+    private function systemUnderTest(string ...$mockedMethods): Config
+    {
+        $mock = $this->getMockBuilder(Config::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods($mockedMethods)
+            ->getMock();
+        return ah::CallConstructor($mock);
     }
 
     #region __construct --------------------------------------------------------
 
     function testConstructor()
     {
-        $config = Config::Instance();
-        $this->assertCount(0, AccessHelper::GetProperty($config, 'options'));
-        $this->assertNull($config->OptionsFilePath());
+        $config = $this->systemUnderTest();
+        $this->assertCount(0, ah::GetProperty($config, 'options'));
+        $this->assertNull(ah::GetProperty($config, 'optionsFilePath'));
     }
 
     #endregion __construct
@@ -59,15 +65,17 @@ class ConfigTest extends TestCase
 
     function testLoadWithNonExistingFile()
     {
-        $this->assertFileDoesNotExist((string)$this->testFilePath);
+        $config = $this->systemUnderTest();
+        $this->assertFileDoesNotExist($this->testFilePath);
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage(
             "Configuration options file not found: $this->testFilePath");
-        Config::Instance()->Load($this->testFilePath);
+        $config->Load($this->testFilePath);
     }
 
     function testLoadWithExistingFile()
     {
+        $config = $this->systemUnderTest();
         $this->createTestFile(<<<PHP
             <?php
             return [
@@ -76,12 +84,14 @@ class ConfigTest extends TestCase
             ];
 
         PHP);
-        $config = Config::Instance();
         $config->Load($this->testFilePath);
-        $this->assertSame($this->testFilePath, $config->OptionsFilePath());
+        $this->assertSame(
+            $this->testFilePath,
+            ah::GetProperty($config, 'optionsFilePath')
+        );
         $this->assertSame(
             ['key1' => 'value1', 'key2' => 'value2'],
-            AccessHelper::GetProperty($config, 'options')->ToArray()
+            ah::GetProperty($config, 'options')->ToArray()
         );
     }
 
@@ -91,13 +101,15 @@ class ConfigTest extends TestCase
 
     function testReloadWithoutLoad()
     {
+        $config = $this->systemUnderTest();
         $this->expectException(\RuntimeException::class);
         $this->expectExceptionMessage('No configuration options file is loaded.');
-        Config::Instance()->Reload();
+        $config->Reload();
     }
 
     function testReload()
     {
+        $config = $this->systemUnderTest();
         $this->createTestFile(<<<PHP
             <?php
             return [
@@ -106,11 +118,10 @@ class ConfigTest extends TestCase
             ];
 
         PHP);
-        $config = Config::Instance();
         $config->Load($this->testFilePath);
         $this->assertSame(
             ['key1' => 'value1', 'key2' => 'value2'],
-            AccessHelper::GetProperty($config, 'options')->ToArray()
+            ah::GetProperty($config, 'options')->ToArray()
         );
         $this->createTestFile(<<<PHP
             <?php
@@ -124,7 +135,7 @@ class ConfigTest extends TestCase
         $config->Reload();
         $this->assertSame(
             ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'],
-            AccessHelper::GetProperty($config, 'options')->ToArray()
+            ah::GetProperty($config, 'options')->ToArray()
         );
     }
 
@@ -134,12 +145,13 @@ class ConfigTest extends TestCase
 
     function testOptionWithoutLoad()
     {
-        $config = Config::Instance();
+        $config = $this->systemUnderTest();
         $this->assertNull($config->Option('key'));
     }
 
     function testOptionWithLoad()
     {
+        $config = $this->systemUnderTest();
         $this->createTestFile(<<<PHP
             <?php
             return [
@@ -148,7 +160,6 @@ class ConfigTest extends TestCase
             ];
 
         PHP);
-        $config = Config::Instance();
         $config->Load($this->testFilePath);
         $this->assertSame('value1', $config->Option('key1'));
         $this->assertSame('value2', $config->Option('key2'));
@@ -161,12 +172,13 @@ class ConfigTest extends TestCase
 
     function testOptionOrDefaultWithoutLoad()
     {
-        $config = Config::Instance();
+        $config = $this->systemUnderTest();
         $this->assertSame('default', $config->OptionOrDefault('key', 'default'));
     }
 
     function testOptionOrDefaultWithLoad()
     {
+        $config = $this->systemUnderTest();
         $this->createTestFile(<<<PHP
             <?php
             return [
@@ -175,7 +187,6 @@ class ConfigTest extends TestCase
             ];
 
         PHP);
-        $config = Config::Instance();
         $config->Load($this->testFilePath);
         $this->assertSame('value1', $config->OptionOrDefault('key1', 'default'));
         $this->assertSame('value2', $config->OptionOrDefault('key2', 'default'));
@@ -188,7 +199,7 @@ class ConfigTest extends TestCase
 
     function testSetOptionWithoutLoad()
     {
-        $config = Config::Instance();
+        $config = $this->systemUnderTest();
         $this->expectException(\RuntimeException::class);
         $this->expectExceptionMessage('Configuration option not found: key1');
         $config->SetOption('key1', 'value1');
@@ -196,6 +207,7 @@ class ConfigTest extends TestCase
 
     function testSetOptionWithNonExistingKey()
     {
+        $config = $this->systemUnderTest();
         $this->createTestFile(<<<PHP
             <?php
             return [
@@ -204,7 +216,6 @@ class ConfigTest extends TestCase
             ];
 
         PHP);
-        $config = Config::Instance();
         $config->Load($this->testFilePath);
         $this->expectException(\RuntimeException::class);
         $this->expectExceptionMessage('Configuration option not found: key3');
@@ -213,6 +224,7 @@ class ConfigTest extends TestCase
 
     function testSetOptionWithTypeMismatch()
     {
+        $config = $this->systemUnderTest();
         $this->createTestFile(<<<PHP
             <?php
             return [
@@ -221,7 +233,6 @@ class ConfigTest extends TestCase
             ];
 
         PHP);
-        $config = Config::Instance();
         $config->Load($this->testFilePath);
         $this->expectException(\RuntimeException::class);
         $this->expectExceptionMessage('Configuration option type mismatch: key1');
@@ -230,6 +241,7 @@ class ConfigTest extends TestCase
 
     function testSetOption()
     {
+        $config = $this->systemUnderTest();
         $this->createTestFile(<<<PHP
             <?php
             return [
@@ -238,7 +250,6 @@ class ConfigTest extends TestCase
             ];
 
         PHP);
-        $config = Config::Instance();
         $config->Load($this->testFilePath);
         $config->SetOption('key1', 'new_value1');
         $this->assertSame('new_value1', $config->Option('key1'));
